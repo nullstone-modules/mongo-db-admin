23f302d8c0ed194f6a68d4e0136256bc04783fba
Scott Sickles <scottsickles@gmail.com>
2023-09-25 17:56:22 -0400

    move into a local variable

--- aws/tf/connection_url.tf
+++ aws/tf/connection_url.tf
@@ -5,6 +5,9 @@ resource "aws_secretsmanager_secret" "db_admin_pg" {
 
 resource "aws_secretsmanager_secret_version" "db_admin_pg" {
   secret_id     = aws_secretsmanager_secret.db_admin_pg.id
+  secret_string = "${var.protocol}://${urlencode(var.username)}:${urlencode(var.password)}@${var.host}${local.port_substring}/${urlencode(var.database)}"
+}
+
+locals {
   port_substring = endswith(var.protocol, "srv") ? "" : ":${var.port}"
-  secret_string = "${var.protocol}://${urlencode(var.username)}:${urlencode(var.password)}@${var.host}/${urlencode(var.database)}"
 }
